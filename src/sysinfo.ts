c4ca1dc0b91dd76c18d7287ac362fb4a4f3b734c
Mahdi <mahdi_salameh@hotmail.com>
2022-01-18 16:08:09 +0100

    /api/v1/sysinfo

--- src/sysinfo.ts
+++ src/sysinfo.ts
@@ -46,9 +46,19 @@ Si.networkInterfaces().then((data) => {
 
 
 
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type' : 'application/json'});
-    res.write(JSON.stringify(result));
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/api/v1/sysinfo') {
+        res.writeHead(200, {'Content-type' : 'application/json'});
+        res.write(JSON.stringify(result));
+    }
+    else{
+        res.writeHead(404,{'Content-Type': 'text/html'});
+        res.write("Page not found (404)\n");
+    }
+
     res.end();
-}).listen(8080);
+});
+
+server.listen(8080);
 
