1785646a88b41c837042ec482e1e3d7266f71ba2
Mahdi <mahdi_salameh@hotmail.com>
2022-01-27 16:33:35 +0100

    test yml

--- src/sysinfo.ts
+++ src/sysinfo.ts
@@ -6,42 +6,42 @@ export const result = [];
 
 Si.cpu().then((data) => {
     result.push({ 'cpu' : data});
-    console.log(result);
+    //console.log(result);
 });
 
 Si.system().then((data) => {
     result.push({ 'system' : data});
-    console.log(result);
+    //console.log(result);
 });
 
 Si.mem().then((data) => {
     result.push({ 'mem' : data});
-    console.log(result);
+    //console.log(result);
 });
 
 Si.osInfo().then((data) => {
     result.push({ 'os' : data});
-    console.log(result);
+    //console.log(result);
 });
 
 Si.currentLoad().then((data) => {
     result.push({ 'currentLoad' : data});
-    console.log(result);
+    //console.log(result);
 });
 
 Si.processes().then((data) => {
     result.push({ 'processes' : data});
-    console.log(result);
+    //console.log(result);
 });
 
 Si.diskLayout().then((data) => {
     result.push({ 'diskLayout' : data});
-    console.log(result);
+    //console.log(result);
 });
 
 Si.networkInterfaces().then((data) => {
     result.push({ 'networkInterfaces' : data});
-    console.log(result);
+    //console.log(result);
 });
 
 
